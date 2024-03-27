8238105ee9d60c268873dbaa24669b74864b7d23
rohansadia1122 <rohansadia1973@gmail.com>
2024-03-27 04:10:46 -0400

    commite
    new app added

---
+++ src/test/java/logingTestFB/LoginTest.java
@@ -0,0 +1,13 @@
+package logingTestFB;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	
+	@Test
+	public void login() {
+		System.out.println("This is loging test");
+	}
+
+}
